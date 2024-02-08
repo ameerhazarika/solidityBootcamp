a0e446c2e690c25f3d31de878430adf0d3016791
ameerhazarika <ameer.hazarika@gmail.com>
2024-02-08 17:06:12 +0530

    Made final changes

--- contracts/proposal.sol
+++ contracts/proposal.sol
@@ -1,9 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 contract ProposalContract { 
-    // Our contract code
+    
     address owner; // address of the owner
     uint256 private counter; // keep track of the ids
+    mapping(uint256 => Proposal) proposal_history; // Recordings of previous proposals
+    mapping(address => bool ) voted_addresses; // Addresses that have voted
+
 
 
             struct Proposal {
@@ -16,9 +19,7 @@ contract ProposalContract {
                 bool current_state; // This shows the current state of the proposal, meaning whether if passes of fails
                 bool is_active; // This shows if others can vote to our contract
             }
-            mapping(uint256 => Proposal) proposal_history; // Recordings of previous proposals
-            mapping(address => bool ) voted_addresses;
-
+          
             // CONSTRUCTOR
              constructor() {
                  owner = msg.sender;
@@ -46,6 +47,7 @@ contract ProposalContract {
             }
 
             function create(string calldata _title,string calldata _description, uint256 _total_vote_to_end) external onlyOwner {
+                require(!proposal_history[counter].is_active, "Existing proposal still active."); // only one proposal at a time
                 counter += 1;
                 proposal_history[counter] = Proposal(_title,_description, 0, 0, 0, _total_vote_to_end, false, true);
             }
@@ -55,10 +57,9 @@ contract ProposalContract {
                     uint256 approve = proposal.approve;
                     uint256 reject = proposal.reject;
                     uint256 pass = proposal.pass;
-                    pass = pass%2==0?pass:pass+1; // if pass votes are odd, add one to it
-                    pass=pass/2;
+                    pass = pass%2==0?pass/2:(pass+1)/2; // if pass votes are odd, add one to it
                     // if approve+(pass/2) votes are more than reject votes, then proposal is approved 
-                    // or if approve greater than reject then it doesn't matter how many pass votes are there
+                    // or if approve votes are greater than reject votes then it doesn't matter how many pass votes are there
                     if (approve>reject || approve+pass>reject) {
                         return true;
                     } else {
@@ -67,28 +68,25 @@ contract ProposalContract {
             }
 
             function vote(uint8 choice) external active newVoter(msg.sender) {
-                // Function logic
-                // First part
+                // choice should be only either of 3 values check
+                require(choice==0||choice==1||choice==2,"Choice should be either 0, 1 0r 2");
                 Proposal storage proposal = proposal_history[counter];
                 uint256 total_vote = proposal.approve + proposal.reject + proposal.pass;
                 voted_addresses[msg.sender]=true;
-                // Second part
+                // removed calculating current state at every if condition
                 if (choice == 1) {
                     proposal.approve += 1;
-                    proposal.current_state = calculateCurrentState();
                 } else if (choice == 2) {
                     proposal.reject += 1;
-                    proposal.current_state = calculateCurrentState();
                 } else if (choice == 0) {
                     proposal.pass += 1;
-                    proposal.current_state = calculateCurrentState();
                 }
-                // updating actual proposal state
-                proposal_history[counter].current_state = proposal.current_state;
-                // Third part
-                if ((proposal.total_vote_to_end - total_vote == 1) && (choice == 1 || choice == 2 || choice == 0)) {
-                    proposal.is_active = false;
-                 //   voted_addresses = [owner]; // reset the addresses that have voted because proposal is inactive and over now
+                // updating actual proposal state , this wasn't done previously and gave wrong proposal state when calling getCurrentProposal 
+                proposal_history[counter].current_state = calculateCurrentState();
+
+                // if total votes exceed votes to end then we close the proposal voting
+                if (proposal.total_vote_to_end - total_vote == 1) {
+                    proposal.is_active = false; 
                 }
             }
 
@@ -96,20 +94,12 @@ contract ProposalContract {
                 proposal_history[counter].is_active = false;
             }
 
-            // function isVoted(address _address) public view returns (bool) {
-            //     for (uint i = 0; i < voted_addresses.length; i++) {
-            //         if (voted_addresses[i] == _address) {
-            //             return true;
-            //         }
-            //     }
-            //     return false;
-            // }
-
             function getCurrentProposal() external view returns(Proposal memory) {
                 return proposal_history[counter];
             }
 
             function getProposal(uint256 number) external view returns(Proposal memory) {
+                require(number>=1 && number<=counter,"Please enter a valid proposal id");
                 return proposal_history[number];
             }
     
